bb1e2f24c5b5a99e39b42d111fe9770064020e9b
specialdesign7777-design <peredreevplaton@gmail.com>
2026-02-20 22:43:09 +0300

    ГААААААААЗ

--- src/page/MMO.jsx
+++ src/page/MMO.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/MMO.module.css";
 import { VscAccount } from "react-icons/vsc";
+import { IoIosArrowDown } from "react-icons/io";
+import ConsultInMain from "../components/ConsultInMain";
+// import list from "../accept/list.svg"
 
 const MMO = () => {
+    const [ShowProgram, SetShowProgram] = useState(false);
+    const [ShowResult, SetShowResult] = useState(false);
+    const [FourReason, SetFourReason] = useState(false);
+
     return (
         <div className={styles.MMO}>
-            <div className={styles.title}>
+            <div className={styles.title_main}>
                 Интенсивная подготовка к Московской матемаческой олимпиаде(ММО)
             </div>
             <div className={styles.block1}>
@@ -26,6 +33,7 @@ const MMO = () => {
                     уровень конкуренции и получите бесценный опыт перед ВсОШ и
                     более серьезными турнирами.
                 </p>
+                {/* <img src={list} alt="" /> */}
             </div>
             <div className={styles.block2}>
                 <h2>Что вас ждет:</h2>
@@ -45,7 +53,7 @@ const MMO = () => {
                     ошибок
                 </p>
             </div>
-            <h1>Интенсив введут</h1>
+            <h1>Интенсив ведут</h1>
             <div className={styles.cards}>
                 {[0, 0, 0].map((e, id) => (
                     <div className={styles.card} key={id}>
@@ -64,6 +72,124 @@ const MMO = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <section className={styles.info}>
+                <div
+                    className={styles.title}
+                    onClick={() => {
+                        if (!ShowProgram) {
+                            SetShowProgram(true);
+                        } else {
+                            SetShowProgram(false);
+                        }
+                    }}
+                >
+                    <p>Программа</p>
+                    <IoIosArrowDown />
+                </div>
+                {ShowProgram ? (
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Alias sequi ratione consectetur molestias iure facere
+                        expedita, obcaecati fugit deleniti? Quia aliquam
+                        corrupti sed voluptates neque reprehenderit quis
+                        cupiditate dicta consequuntur! Fuga a sequi hic illum
+                        nobis placeat voluptatibus, magnam accusantium eum
+                        fugiat tenetur quasi expedita accusamus animi
+                        architecto, vel tempore, officiis omnis! Atque quia,
+                        harum ducimus sequi fugit rerum et. Cupiditate ex ullam
+                        aperiam cumque fuga dolor dignissimos repudiandae est
+                        sed iusto incidunt recusandae quaerat quia accusantium
+                        architecto, neque, eum voluptatem, molestiae fugit
+                        voluptate laborum eveniet? Debitis dignissimos cumque
+                        quidem! Laboriosam quam, sed blanditiis obcaecati minima
+                        minus ipsam excepturi ad veniam earum magni odio
+                        veritatis sapiente iusto. Voluptatum reprehenderit quae,
+                        ex laudantium hic impedit accusamus, consequatur tenetur
+                        quidem, modi repellendus.
+                    </p>
+                ) : null}
+            </section>
+            <section className={styles.info}>
+                <div
+                    className={styles.title}
+                    onClick={() => {
+                        if (!ShowResult) {
+                            SetShowResult(true);
+                        } else {
+                            SetShowResult(false);
+                        }
+                    }}
+                >
+                    <p>Результат</p>
+                    <IoIosArrowDown />
+                </div>
+                {ShowResult ? (
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Alias sequi ratione consectetur molestias iure facere
+                        expedita, obcaecati fugit deleniti? Quia aliquam
+                        corrupti sed voluptates neque reprehenderit quis
+                        cupiditate dicta consequuntur! Fuga a sequi hic illum
+                        nobis placeat voluptatibus, magnam accusantium eum
+                        fugiat tenetur quasi expedita accusamus animi
+                        architecto, vel tempore, officiis omnis! Atque quia,
+                        harum ducimus sequi fugit rerum et. Cupiditate ex ullam
+                        aperiam cumque fuga dolor dignissimos repudiandae est
+                        sed iusto incidunt recusandae quaerat quia accusantium
+                        architecto, neque, eum voluptatem, molestiae fugit
+                        voluptate laborum eveniet? Debitis dignissimos cumque
+                        quidem! Laboriosam quam, sed blanditiis obcaecati minima
+                        minus ipsam excepturi ad veniam earum magni odio
+                        veritatis sapiente iusto. Voluptatum reprehenderit quae,
+                        ex laudantium hic impedit accusamus, consequatur tenetur
+                        quidem, modi repellendus.
+                    </p>
+                ) : null}
+            </section>
+            <section className={styles.info} style={{marginBottom: 100}}>
+                <div
+                    className={styles.title}
+                    onClick={() => {
+                        if (!FourReason) {
+                            SetFourReason(true);
+                        } else {
+                            SetFourReason(false);
+                        }
+                    }}
+                >
+                    <p>4 причины выбрать ka.best</p>
+                    <IoIosArrowDown />
+                </div>
+                {FourReason ? (
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Alias sequi ratione consectetur molestias iure facere
+                        expedita, obcaecati fugit deleniti? Quia aliquam
+                        corrupti sed voluptates neque reprehenderit quis
+                        cupiditate dicta consequuntur! Fuga a sequi hic illum
+                        nobis placeat voluptatibus, magnam accusantium eum
+                        fugiat tenetur quasi expedita accusamus animi
+                        architecto, vel tempore, officiis omnis! Atque quia,
+                        harum ducimus sequi fugit rerum et. Cupiditate ex ullam
+                        aperiam cumque fuga dolor dignissimos repudiandae est
+                        sed iusto incidunt recusandae quaerat quia accusantium
+                        architecto, neque, eum voluptatem, molestiae fugit
+                        voluptate laborum eveniet? Debitis dignissimos cumque
+                        quidem! Laboriosam quam, sed blanditiis obcaecati minima
+                        minus ipsam excepturi ad veniam earum magni odio
+                        veritatis sapiente iusto. Voluptatum reprehenderit quae,
+                        ex laudantium hic impedit accusamus, consequatur tenetur
+                        quidem, modi repellendus.
+                    </p>
+                ) : null}
+            </section>
+
+            <div className={styles.for_how}>
+                <div className={styles.title_main} style={{textAlign: 'center'}}>
+                    Чтобы задать свои вопросы или записаться на курс - пишите нам!
+                </div>
+                <ConsultInMain/>
             </div>
         </div>
     );
